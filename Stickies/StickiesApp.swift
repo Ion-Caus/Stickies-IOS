6fa143b1487f7ba51973470afb504d81909d5105
Ion-Caus <causion01@gmail.com>
2022-05-29 00:11:18 +0200

    Initial Commit

---
+++ Stickies/StickiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickiesApp.swift
+//  Stickies
+//
+//  Created by Ion Caus on 29.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct StickiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
